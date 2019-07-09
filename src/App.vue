3ad726094a4e0e7e09bac0c7dd6e9d211f08fd43
kei <dlf42139@163.com>
2019-07-09 20:32:39 +0800

    删除不需要的自带实例代码

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 // 导出
 export default {
   name: 'app',
   // 注册子组件
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
