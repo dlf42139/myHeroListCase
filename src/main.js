557023bb662d35c0152e246f6590fb13806b0afb
dlf42139 <dlf42139@163.com>
2019-07-10 15:59:36 +0800

    在app.vue中完成首页

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 // 值为false就是不提示现在是开发环境
 Vue.config.productionTip = false
 
+// 导入样式文件
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
 // 把App组件，渲染，挂载到#app（会替换掉网页里的#app），是我们的根组件
 new Vue({
   render: h => h(App),
