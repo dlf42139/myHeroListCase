82d2c802705ae8e7040f762e7bc522d9a98f7a6e
dlf42139 <dlf42139@163.com>
2019-07-10 22:08:04 +0800

    配置路由对象vue-router

--- src/main.js
+++ src/main.js
@@ -8,7 +8,24 @@ Vue.config.productionTip = false
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
 
+// 导入vue-router
+// 导入node_modules里的不用写具体路径
+// 只导入是不行的，要注册 
+import VueRouter from 'vue-router'
+// 配置vue的插件vue-router
+Vue.use(VueRouter);
+
+// 创建路由对象
+const router = new VueRouter({
+  // 配置路由规则
+  routes: [
+
+  ]
+})
+
 // 把App组件，渲染，挂载到#app（会替换掉网页里的#app），是我们的根组件
 new Vue({
   render: h => h(App),
+  // 配置路由对象
+  router,
 }).$mount('#app')
