e408fb879d888932282f4d22ca36c002c35f59ab
dlf42139 <dlf42139@163.com>
2019-07-11 17:32:04 +0800

    完成添加英雄功能

--- src/views/heros/AddHero.vue
+++ src/views/heros/AddHero.vue
@@ -4,25 +4,51 @@
     <form>
       <div class="form-group">
         <label for="heroname">姓名</label>
-        <input type="email" class="form-control" id="heroname" placeholder="姓名" />
+        <input type="text" v-model="formData.name" class="form-control" id="heroname" placeholder="姓名" />
       </div>
       <div class="form-group">
         <label for="herogender">性别</label>
-        <input
-          type="password"
-          class="form-control"
-          id="herogender"
-          placeholder="男"
-        />
+        <select id="herogender" v-model="formData.gender" class="form-control">
+          <option value="男">男</option>
+          <option value="女">女</option>
+        </select>
+        <!-- <input class="form-control" v-model="formData.gender" id="herogender" placeholder="男" /> -->
       </div>
-
-      <button type="submit" class="btn btn-success">提交</button>
+      
+      <input type="button" value="提交" class="btn btn-success" @click="addItem">
+      <!-- <button class="btn btn-success" @click.prevent="addItem">提交</button> -->
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+
+export default {
+  data(){
+    return {
+      formData:{
+        name:'',
+        gender:'男',
+      }
+    }
+  },
+  methods: {
+    addItem(){
+      // console.log(this.formData);
+      axios
+      .post('http://localhost:3000/heros',this.formData)
+      .then( res=>{
+        // console.log(res);
+        let status = res.status;
+        if (status == 201) {
+          // location.href = '/herolist'
+          this.$router.push('/herolist');
+        }
+      } )
+    }
+  },
+};
 </script>
 
 <style>
