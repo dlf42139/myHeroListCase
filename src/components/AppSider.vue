224ca43dcc9a296367fc8d4ae346c7bf12d13210
dlf42139 <dlf42139@163.com>
2019-07-11 08:53:26 +0800

    提取中间那一大块的英雄列表组件，配置路由

--- src/components/AppSider.vue
+++ src/components/AppSider.vue
@@ -2,12 +2,13 @@
   <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
       <li class="active">
-        <a href="#">
-          英雄列表
-        </a>
+        <router-link to="/herolist">英雄列表</router-link>
       </li>
+      <!-- <router-link to="" tag="li">
+        <a href="">武器列表</a>
+      </router-link> -->
       <li>
-        <a href="#">武器列表</a>
+        <router-link to="">武器列表</router-link>
       </li>
       <li>
         <a href="#">装备列表</a>
